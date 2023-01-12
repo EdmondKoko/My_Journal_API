c32500cd7b4432c74845e9f7a0b41d0fbbbb8297
Руслан Трофимов <edmondkoko777@gmail.com>
2023-01-12 14:54:26 +0300

    Финальный проект спринта: API для Yatube

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from api.permissions import IsAuthorOrReadOnly
-from posts.models import Post, Group
+from api.permissions import IsOwnerOrReadOnly
 from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Post, Group
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -18,15 +19,17 @@ class PostViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        return post.comments
+        second_queryset = post.comments.all()
+        return second_queryset
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
